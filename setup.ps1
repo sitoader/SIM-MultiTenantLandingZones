c547390976c16699c019b6d7f268b035c9cfc29c
Jesper Fütterer Bing <jj@solvoit.com>
2021-12-07 21:53:11 +0100

    Added cloudpuzzles

--- setup.ps1
+++ setup.ps1
@@ -3,8 +3,8 @@
  cd .\src\platform-automation
  Add-Azaccount
  Select-AzSubscription -SubscriptionId "ccc92f0c-83ac-42a0-ba0f-ee96f8986f94"
- $rg = New-AzResourceGroup -resourcegroupname 'sim-test-deployment' -location 'west europe'
- $deploy = new-azresourcegroupdeployment -resourcegroup $rg.ResourceGroupName -templateFile .\artifacts\templates\resourcegroup\StorageAccount.json -storageAccountNamePrefix 'simdevops' -verbose
+ $rg = New-AzResourceGroup -resourcegroupname 'sim-prod-deployment' -location 'west europe'
+ $deploy = new-azresourcegroupdeployment -resourcegroup $rg.ResourceGroupName -templateFile .\artifacts\templates\resourcegroup\StorageAccount.json -storageAccountNamePrefix 'simpdevops' -verbose
  # Output names & key     
  $deploy.outputs.storageAccountName.value     
  $deploy.outputs.masterKey.value
